5b71125af5dc2056bc09d6731ed7491de69afe1b
arturgspb <art@realweb.ru>
2018-10-31 13:42:26 +0300

    init

--- metaendpoints/apiserver.py
+++ metaendpoints/apiserver.py
@@ -7,7 +7,18 @@ from grpc_health.v1 import health_pb2, health_pb2_grpc
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 
-def serve(register_servicers_callbacks = [], port: int = 50051, grace_period: int = 5):
+def serve(register_servicers_callbacks=None, port: int = 50051, grace_period: int = 10):
+    """
+    Старт GRPC сервера
+
+    :param register_servicers_callbacks: Список колбеков для регистрации сервисов апи в grpc сервере
+    :param port: grpc порт. Менять не желательно
+    :param grace_period: Период ожидания завершения подключений (https://grpc.io/grpc/python/grpc.html#grpc.Server.stop)
+    :return:
+    """
+    if register_servicers_callbacks is None:
+        register_servicers_callbacks = []
+
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     server.add_insecure_port('[::]:{}'.format(port))
 
