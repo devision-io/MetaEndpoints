30f873de97b1c775c736ab844f9316a63a16280f
arturgspb <art@realweb.ru>
2020-04-22 17:10:17 +0300

    [~] Улучшена обработка ошибок на верхнем уровне

--- metaendpoints/apimethod.py
+++ metaendpoints/apimethod.py
@@ -28,35 +28,36 @@ class Api(object):
         decorator_self = self
 
         def wrappee(*args, **kwargs):
-            request = args[1]
-            context = args[2]
-
-            context.user_id = None
-            context.is_dev = None
-            user_info = None
-            imd = context.invocation_metadata()
-            context.metadata = {}
-
-            # Копируем весь imd в виде словаря, это нужно для совместимости с библиотекой grpc-testing
-            for metadatum_object in imd:
-                context.metadata.update({metadatum_object.key: metadatum_object.value})
-
-            for md in imd:
-                if md.key == 'x-endpoint-api-userinfo':
-                    user_info = json.loads(base64.b64decode(md.value))
-
-            if user_info:
-                claims = json.loads(user_info.get("claims", {}))
-                token_scopes = claims.get("scope").split(' ')  # Эти данные не нужны в самом сервисе, так как не должы определять бизнеслогику.
-                context.is_dev = 'meta.dev' in token_scopes
-                context.user_id = user_info.get("id")
-                context.metadata.update({"user_id": context.user_id, "is_dev": context.is_dev})
-                if not any((True for x in token_scopes if x in decorator_self.scopes)):
-                    err_msg = 'Token expected any of scopes for this method: [' + ', '.join(decorator_self.scopes) + "]"
-                    context.abort(PERMISSION_DENIED_STATUS_CODE, err_msg)
             try:
+                request = args[1]
+                context = args[2]
+
+                context.user_id = None
+                context.is_dev = None
+                user_info = None
+                imd = context.invocation_metadata()
+                context.metadata = {}
+
+                # Копируем весь imd в виде словаря, это нужно для совместимости с библиотекой grpc-testing
+                for metadatum_object in imd:
+                    context.metadata.update({metadatum_object.key: metadatum_object.value})
+
+                for md in imd:
+                    if md.key == 'x-endpoint-api-userinfo':
+                        user_info = json.loads(base64.b64decode(md.value))
+
+                if user_info:
+                    claims = json.loads(user_info.get("claims", {}))
+                    token_scopes = claims.get("scope").split(' ')  # Эти данные не нужны в самом сервисе, так как не должы определять бизнеслогику.
+                    context.is_dev = 'meta.dev' in token_scopes
+                    context.user_id = user_info.get("id")
+                    context.metadata.update({"user_id": context.user_id, "is_dev": context.is_dev})
+                    if not any((True for x in token_scopes if x in decorator_self.scopes)):
+                        err_msg = 'Token expected any of scopes for this method: [' + ', '.join(decorator_self.scopes) + "]"
+                        context.abort(PERMISSION_DENIED_STATUS_CODE, err_msg)
                 return original_func(*args, **kwargs)
             except Exception as e:
+                raise500 = False
                 try:
                     if context._state and context._state.aborted:
                         err_request_ = ""
@@ -76,10 +77,18 @@ class Api(object):
                             meta_log.error(err_msg, err_ctx)
                         else:
                             meta_log.warning(err_msg, err_ctx)
+                    else:
+                        raise500 = True
+                        meta_log.error("Unhandled non context exception", {
+                            "e": e
+                        })
                 except Exception as e:
+                    raise500 = True
                     meta_log.error("Unable to log grpc abort error", {
                         "e": e
                     })
+                if raise500:
+                    context.abort(INTERNAL_ERROR_STATUS_CODE, "Unexpected server error")
                 raise e
 
         return wrappee
